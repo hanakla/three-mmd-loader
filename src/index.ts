97076c0bc5c244b93c68048abe671fb0dc71d0bb
Ragg <Ragg-@users.noreply.github.com>
2017-03-28 23:13:56 +0900

    Export MMDPhysics

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import MMDLoader from './MMDLoader';
 import MMDAudioManager from './MMDAudioManager';
 import MMDGrantSolver from './MMDGrantSolver';
 import MMDHelper from './MMDHelper';
+import MMDPhysics from './MMDPhysics';
 
 export {default as MMDLoader} from './MMDLoader';
 export {default as MMDAudioManager} from './MMDAudioManager';
@@ -14,9 +15,12 @@ export {default as QuaternionKeyframeTrackEx} from './QuaternionKeyframeTrackEx'
 export {default as NumberKeyframeTrackEx} from './NumberKeyframeTrackEx';
 export {default as CubicBezierInterpolation} from './CubicBezierInterpolation';
 
+export {default as MMDPhysics} from './MMDPhysics'
+
 export const mixin = (THREE: any) => {
     THREE.MMDLoader = MMDLoader;
     THREE.MMDAudioManager = MMDAudioManager;
     THREE.MMDGrantSolver = MMDGrantSolver;
     THREE.MMDHelper = MMDHelper;
+    THREE.MMDPhysics = MMDPhysics;
 }
